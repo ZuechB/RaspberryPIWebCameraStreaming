aac1bcf07dea1fd183e9ca748d7ad882fbae9701
Brandon Zuech <Brandonzuech@hotmail.com>
2025-03-03 18:46:26 -0500

    test

--- streamCamera.py
+++ streamCamera.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 # Configuration
-rtsp_server_url = "rtsp://0.0.0.0:8554/stream"
+rtsp_server_url = "rtsp://192.168.0.162:8554/stream"
 video_device = "/dev/video0"
 
 # Check if ffmpeg is installed
